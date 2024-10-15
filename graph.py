9e26e7305991117f94420fa80ea1cb61bc239906
Daniel Bhatti <dbhatti@haverford.edu>
2024-10-15 11:53:11 -0400

    updates some functions for the graph

--- graph.py
+++ graph.py
@@ -1,15 +1,45 @@
 """
-Name:
-Date:
-Description:
+Name: Daniel Bhatti
+Date: 10/14/24
+Description: 
 """
 
-class Graph:
+import numpy as np
 
-    def __init__(self):
+class Network:
+
+    def __init__(self, data):
         """
-        Description: Abstracts station to a graph with node and edges; graph is represented as adjacency matrix;
+        Description: Abstracts Network to an adjacency matrix with node and edges; graph is represented as adjacency matrix;
         Inputs: nodes and edges, passed in as a dictionary with stations and their connections
         return:  
         """
+        self.stations = {}; self.lines = {}
+        for n in range(data.shape[0]):
+            self.stations[n] = data.iat[n,5]
+            self.lines[n] = data.iat[n,8]
+            
+        # stations are considered adjacent if they share one or more lines between them
+        self.adjacency_matrix = np.zeros((len(self.stations), len(self.stations)))
+        for i in range(len(self.stations)):
+            for j in range(len(self.stations)):
+                if self.shares_element(self.lines[i], self.lines[j]):
+                    self.adjacency_matrix[i][j] = 1
+
+
+    def min_spanning_tree(self, node_a, node_b):
         pass
+
+    ## taken from Google search labs Generative AI, Oct 15 2024
+    def shares_element(self, str1, str2):
+        for char in str1:
+            if char in str2:
+                return True
+        return False
+
+     
+
+
+        
+
+        
--- main.py
+++ main.py
@@ -9,14 +9,46 @@ to run test quick: python3 main.py data/MTA_Subway_Stations_20241013.csv
 import argparse
 import numpy as np
 import pandas as pd
+import geopandas as gpd
+from shapely.geometry import Point; import networkx as nx; import matplotlib.pyplot as plt
+import graph
+import networkx as nx
 
 def main():
     args = parse_args()
     network_filename = args.network_filename
     network = pd.read_csv(network_filename)
-    print(process_data(network))
+    filtered_network = process_data(network)
+    print(filtered_network)
     
-    
+
+    network = graph.Network(filtered_network)
+
+    print(network.stations)
+    print(network.lines)
+    print(network.adjacency_matrix)
+
+    # copied for demonstration
+    """
+    geometry = [Point(xy) for xy in zip(filtered_network["GTFS Longitude"], 
+                                        filtered_network["GTFS Latitude"])]
+
+    station_points = gpd.GeoDataFrame(filtered_network, geometry=geometry, crs = {'init': 'epsg:2263'})
+    station_points.plot()
+    plt.show()
+    """
+    # copied for demonstration
+    G = nx.Graph()
+
+    for i in range(network.adjacency_matrix.shape[0]): 
+        for j in range(network.adjacency_matrix.shape[1]): 
+            if network.adjacency_matrix[i][j] == 1: 
+                G.add_edge(i,j) 
+
+    nx.draw( G ) 
+    plt.show()      
+
+
 
 def parse_args():
     """Parse command line arguments (build-graph files)."""
@@ -26,9 +58,13 @@ def parse_args():
     return args
 
 
+
 def process_data(data):
-    data[data['CBD'] == True]
-    return data
+    refinement = data[data['CBD'] == True]
+    #refinement = refinement[refinement['Daytime Routes'].str.contains('6')]
+    return refinement
+
+
 
 def read_csv(filename):
     """Read .csv into graph format"""
